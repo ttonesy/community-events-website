66a46e5b6dfd4e758256872688425a725943dc11
Mike <mikey34@nmsu.edu>
2024-05-02 23:18:30 -0600

    new changes to authenication and login/logout

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,18 +1,45 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [username, setUsername] = useState(''); 
+    useEffect(() => {
+      fetch('http://localhost:4000/profile', {
+        credentials: 'include',
+      }).then(response => {
+        response.json().then(userInfo => {
+          setUsername(userInfo.username);     
+        }); 
+      });
+    }, []); 
+
+    function logout() {
+      fetch('http://localhost:4000/logout', {
+        credentials: 'include',
+        method: 'POST', 
+      });
+      setUsername(null); 
+    }
+
+
+
+
   return (
     <header>
-      <Link to="/" className="logo">
-        MyBlog
-      </Link>
+      <Link to="/" className="logo"> MyBlog </Link>
       <nav>
-        <Link to="/login" className="login">
-          Login
-        </Link>
-        <Link to="/register" className="register">
-          Register
-        </Link>
+        {username && (
+          <> 
+          <Link to = "/create">Create new post</Link>
+          <a onClick = {logout}>Logout</a>
+          </>
+        )}
+        {!username && (
+          <>
+           <Link to="/login" className="login"> Login</Link>
+        <Link to="/register" className="register"> Register </Link>
+          </>
+        )}
       </nav>
     </header>
   );
